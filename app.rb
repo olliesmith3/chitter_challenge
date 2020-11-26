0d0193416d65280809c49b354a6e3e930644b6f8
Ollie <oliverdanielsmith@hotmail.com>
2020-11-26 16:08:19 +0000

    Added test for sign up - not linked to database yet

--- app.rb
+++ app.rb
@@ -5,5 +5,15 @@ class Chitter < Sinatra::Base
     erb :index
   end
 
+  get '/users/new' do
+    erb :sign_up
+  end
+
+  post '/users' do
+    # User.create(params[:email], params[:first_name], params[last_name], params[:password])
+    redirect '/'
+
+  end
+
   run! if app_file == $0
 end
